802d986407c2044ebac6a2938c8a4f9a8d177e2c
Dunamis <cardano.liberty@gmail.com>
2024-11-14 19:54:57 -0600

    base test case

---
+++ tests/TestCase/UnitTestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Liberty\System\Test\TestCase;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class UnitTestCase
+ */
+abstract class UnitTestCase extends TestCase
+{
+}
